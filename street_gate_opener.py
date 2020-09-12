7d02806654626dc53730834d6ffbe31bff600a11
Daniel Rogozan <dan.rogozan@tora.com>
2020-09-12 13:27:41 +0300

    12092020

--- street_gate_opener.py
+++ street_gate_opener.py
@@ -1,12 +1,25 @@
+import configparser
 import RPi.GPIO as GPIO
-
-NUMBERS = ("0743144113")
-GATE_GPIO = 18
-
+from time import sleep
 
 class StreetGateOpener:
 
     def __init__(self):
+        self.config = configparser.ConfigParser()
+        self.config.read('config.ini')
+        self.gate_gpio = self.config['DEFAULT']['GateGpio']
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(GATE_GPIO, GPIO.OUT)
+        GPIO.setup(self.gate_gpio, GPIO.OUT)
+
+    def open_gate(self):
+        GPIO.output(self.gate_gpio, GPIO.LOW)
+        sleep(2)
+        GPIO.output(self.shelf1, GPIO.HIGH)
+
+
+if __name__ == '__main__':
+    x = StreetGateOpener()
+    print(x.config['DEFAULT']['GateGpio'])
+    x.gate_gpio
+
 
