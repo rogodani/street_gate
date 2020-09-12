2c27868003f965216191c8b77c20fc0fcdd5e889
Daniel Rogozan <dan.rogozan@tora.com>
2020-09-12 14:54:28 +0300

    12092020

--- street_gate_opener.py
+++ street_gate_opener.py
@@ -2,6 +2,7 @@ import configparser
 import RPi.GPIO as GPIO
 from time import sleep
 
+
 class StreetGateOpener:
 
     def __init__(self):
@@ -9,13 +10,13 @@ class StreetGateOpener:
         self.config.read('config.ini')
         self.gate_gpio = int(self.config['DEFAULT']['GateGpio'])
         self.phone_book = self.config['access']['PhoneNumbers'].split(',')
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.gate_gpio, GPIO.OUT)
 
     def phone_number_validation(self, number):
         return number[-11:-1] in self.phone_book
 
     def open_gate(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.gate_gpio, GPIO.OUT)
         GPIO.output(self.gate_gpio, GPIO.LOW)
         sleep(2)
         GPIO.output(self.gate_gpio, GPIO.HIGH)
@@ -23,9 +24,6 @@ class StreetGateOpener:
         GPIO.cleanup()
 
 
-
 if __name__ == '__main__':
     x = StreetGateOpener()
     x.open_gate()
-
-
